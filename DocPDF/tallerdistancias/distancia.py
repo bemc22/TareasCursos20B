c1c2a4b768f4facb1626e878c9b19feb57ca1958
Enmartz <Enmartz@hotmail.com>
2022-03-17 08:49:32 -0500

    update distancia.py
    first functional version

--- DocPDF/tallerdistancias/distancia.py
+++ DocPDF/tallerdistancias/distancia.py
@@ -1,33 +1,42 @@
 # import libraries
 
 import os
+from os import listdir
+from os.path import isfile, join
+
+import time
+from datetime import datetime
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-# load data
+pd.option_context('display.max_rows', None, 'display.max_columns', None)
+
+# load reference data
 
 reference_path = Path('datos/Datos Estaciones AMB.xlsx')
-reference_data = pd.read_excel(reference_path, sheet_name="Normal", skiprows=[1])
-reference_pm25 = reference_data["PM2.5"]
-reference_pm25 = reference_pm25.apply(lambda x: pd.to_numeric(x, errors='coerce')).dropna()
+reference_data = pd.read_excel(reference_path, sheet_name="Normal", skiprows=[1], index_col='Date&Time',
+                               usecols=['Date&Time', 'PM2.5'])
+reference_data['PM2.5'] = reference_data['PM2.5'].apply(lambda x: pd.to_numeric(x, errors='coerce')).dropna()
+reference_data.index.name = 'DateTime'
+reference_data = reference_data.rename(columns={'PM2.5': 'reference'})
 
-measure_path = Path('datos/mediciones_clg_normalsup_pm25_a_2018-11-01T00_00_00_2018-11-30T23_59_59.csv')
-measure_data = pd.read_csv(measure_path)
-measure_data = measure_data.sort_values(by='fecha_hora_med', ascending=True)
-measure_pm25 = measure_data['valor']
-measure_pm25 = measure_pm25.apply(lambda x: pd.to_numeric(x, errors='coerce')).dropna()
+# load measure data
 
-# first computation
+measure_path = Path('datos/mediciones/mediciones_clg_normalsup_pm25_a_2018-11-01T00_00_00_2018-11-30T23_59_59.csv')
+measure_data = pd.read_csv(measure_path, index_col='fecha_hora_med', usecols=['fecha_hora_med', 'valor'])
+measure_data.index = pd.to_datetime(measure_data.index, format='%Y-%m-%dT%H:%M:%S.%fZ')
+measure_data['valor'] = measure_data['valor'].apply(lambda x: pd.to_numeric(x, errors='coerce')).dropna()
+measure_data.index.name = 'DateTime'
+measure_data = measure_data.rename(columns={'valor': 'measure'})
 
-reference_rolling = reference_pm25.rolling(window=1000).mean()
-reference_rolling.plot(), plt.title('reference moving average'), plt.show()
-measure_rolling = measure_pm25.rolling(window=1000).mean()
-measure_rolling.plot(), plt.title('measure moving average'), plt.show()
+# interleave data
 
-interleave_pm25 = pd.concat([reference_pm25, measure_pm25])
+interleave_data = pd.concat([reference_data, measure_data])
+interleave_data = interleave_data.sort_index()
 
+interleave_data.plot(), plt.show()
 
-print(reference_data["PM2.5"])
+print('Final feliz')
